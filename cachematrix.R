50cf7ca34a9a9979642534f50a9f3b4173a944ad
Rajayyagari <raj@objectbasetek.com>
2017-02-05 16:30:28 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,17 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(inputMatrix = matrix()) {
+  cacheInvMatrix <- NULL
+  set <- function (y)
+  {
+    inputMatrix <<- y
+    cacheInvMatrix <<- NULL
+  }
+  get <- function () inputMatrix
+  setInvMatrix <- function (invMatrixResult) cacheInvMatrix <<- invMatrixResult
+  getInvMatrix <- function () cacheInvMatrix
+  list (set = set, get = get, setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 
 }
 
@@ -11,5 +21,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  print (environment())
+  ## Return a matrix that is the inverse of 'x'
+  invertedMatrix <- x$getInvMatrix()
+  if (!is.null(invertedMatrix))
+    {
+      message("Inverted Matrix from Cache")
+      return (invertedMatrix)
+    }
+  invertedMatrix <- solve(x$get(),...)
+  x$setInvMatrix(invertedMatrix)
+  invertedMatrix
+
 }
